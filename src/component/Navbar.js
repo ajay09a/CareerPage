0d6ae2f8c141a82b785b72810c0d18c93cfcea53
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-08-09 21:59:48 +0530

    styled navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from '../styles/navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div className='navbar-container'>
+    <div className={styles.navbarContainer}>
         <div className='logo'>
             <img src='https://getpeppermint.co/wp-content/uploads/2017/05/Untitled-design-86-1.png' alt='logo' />
         </div>
