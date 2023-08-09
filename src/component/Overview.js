9d4a74003fa87c4851b220f1630c7963b54b209e
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-08-10 01:18:25 +0530

    fix some styling

--- src/component/Overview.js
+++ src/component/Overview.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Overview = () => {
   return (
-    <div>
+    <div style={{padding:"10px 30px"}}>
         <h1>Career</h1>
         <h2>About Peppermint</h2>
         <p>Peppermint is an award-winning robotics company, supported by SINE IIT-Bombay and Qualcomm. Peppermint develops and deploys Industrial and Enterprise robots and platforms for mobility-led services. The Engineering team has 50+ years of experience in building deep-tech and robotics products. Robots built on the Peppermint Platform are deployed across 13 cities in 4 countries!</p>
