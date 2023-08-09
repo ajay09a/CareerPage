c9e029fb18b7a6c382a8ed6e3a60e1858387097c
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-08-09 21:46:09 +0530

    create navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+        <div className='logo'>
+            <img src='https://getpeppermint.co/wp-content/uploads/2017/05/Untitled-design-86-1.png' alt='logo' />
+        </div>
+        <ul>
+            <li>Over-view</li>
+            <li>Opportunity</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
