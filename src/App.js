f10f59571eafd5da55d44af55bc70d3cb8ef860d
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-08-09 22:45:09 +0530

    create overview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from './component/Hero';
 import Navbar from './component/Navbar';
+import Overview from './component/Overview';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Hero />
+      <Overview />
     </>
   );
 }
---
+++ src/component/Overview.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Overview = () => {
+  return (
+    <div>
+        <h1>Career</h1>
+        <h2>About Peppermint</h2>
+        <p>Peppermint is an award-winning robotics company, supported by SINE IIT-Bombay and Qualcomm. Peppermint develops and deploys Industrial and Enterprise robots and platforms for mobility-led services. The Engineering team has 50+ years of experience in building deep-tech and robotics products. Robots built on the Peppermint Platform are deployed across 13 cities in 4 countries!</p>
+        <hr />
+    </div>
+  )
+}
+
+export default Overview
